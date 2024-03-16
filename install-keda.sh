c41b9434b6d40c17733360cd2c7f2d2a3df21f90
Richa Mishra <richa.mishra@smarsh.com>
2024-03-16 22:46:04 +0530

    updated few files

--- install-keda.sh
+++ install-keda.sh
@@ -2,4 +2,4 @@
 helm repo add kedacore https://kedacore.github.io/charts
 helm repo update
 kubectl create namespace keda
-helm install keda kedacore/keda --namespace keda
+helm upgrade --install keda kedacore/keda --namespace keda
